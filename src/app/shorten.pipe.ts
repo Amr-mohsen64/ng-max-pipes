6d37619b1ae29a5d0a54fbb6bd94c5b898a168dc
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-10-09 03:43:16 +0200

    added custom pipe -> shorten

---
+++ src/app/shorten.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "shorten",
+})
+export class ShortenPipe implements PipeTransform {
+  transform(value: string, limit: number) {
+    if (value.length > 10) return value.substring(0, limit=10) + "...";
+    return value;
+  }
+}
